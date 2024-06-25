a86e333ab187c2bb965c9848baebbe276d6c9078
Lucio neves <luciocecilia20@gmail.com>
2024-06-25 15:36:23 -0400

    feat: adiciona roteamento  para createComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './feature/list/list.component';
+import { CreateComponent } from './feature/create/create.component';
 
-export const routes: Routes = [{
-    path: '', component: ListComponent,
-}];
+export const routes: Routes = [
+    { path: '', component: ListComponent,},
+    { path: 'create-product', component: CreateComponent},
+    
+
+];
