aeeca70452565e6306916bacbada403514cb73b9
Lucio neves <luciocecilia20@gmail.com>
2024-07-02 20:46:25 -0400

    feat: adiciona componente NoItems na lista

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -6,19 +6,23 @@ import { CardComponent } from './components/card/card.component';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
+import { NoItemsComponent } from './no-items/no-items.component';
 
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
   
-  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+     
+  );
 
   productsService = inject(ProductsService);
   router = inject(Router);
